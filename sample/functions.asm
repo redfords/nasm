c41cf9e11f7d55608caccafbd87e77f0ccbccdee
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-11-08 22:47:22 -0300

    Add sprintLF

--- sample/functions.asm
+++ sample/functions.asm
@@ -15,8 +15,7 @@ finished:
     sub     eax, ebx
     pop     ebx
     ret
- 
- 
+
 ;------------------------------------------
 ; void sprint(String message)
 ; String printing function
@@ -40,6 +39,20 @@ sprint:
     pop     edx
     ret
  
+;------------------------------------------
+; void sprintLF(String message)
+; String printing with line feed function
+sprintLF:
+    call    sprint
+ 
+    push    eax         ; push eax onto the stack to preserve it while we use the eax register in this function
+    mov     eax, 0Ah    ; move 0Ah into eax - 0Ah is the ascii character for a linefeed
+    push    eax         ; push the linefeed onto the stack so we can get the address
+    mov     eax, esp    ; move the address of the current stack pointer into eax for sprint
+    call    sprint      ; call our sprint function
+    pop     eax         ; remove our linefeed character from the stack
+    pop     eax         ; restore the original value of eax before our function was called
+    ret                 ; return to our program
  
 ;------------------------------------------
 ; void exit()
