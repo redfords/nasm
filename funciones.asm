f5bf853d3944c4f72d2ac3c0ef504ec76c3b5873
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-11-14 11:25:17 -0300

    Add functions

--- funciones.asm
+++ funciones.asm
@@ -7,8 +7,21 @@ leerCadena:
         add     esp, 4
         ret
 
+leerCadenaTexto:
+        push    cadenaTexto
+        call    gets
+        add     esp, 4
+        ret 
+
 mostrarCadena:
         push    cadena
+        push    fmtString
+        call    printf
+        add     esp, 8
+        ret
+
+mostrarCadenaTexto:
+        push    cadenaTexto
         push    fmtString
         call    printf
         add     esp, 8
@@ -19,6 +32,13 @@ mostrarCadena:
 
 leerNumero:
         push    numero
+        push    fmtInt
+        call    scanf
+        add     esp, 8
+        ret
+
+leerNumero6:
+        push    auxBucle
         push    fmtInt
         call    scanf
         add     esp, 8
@@ -38,8 +58,52 @@ mostrarDivisor:
         add     esp, 8
         ret
 
+mostrarPromedio:
+        push    dword [promedio]
+        push    fmtInt
+        call    printf
+        add     esp, 8
+        ret
+
+mostrarNumeroPar:
+        push    dword [numeroPar]
+        push    fmtInt
+        call    printf
+        add     esp, 8
+        ret
+
+mostrarColumnas:
+        push    dword [columnas]
+        push    fmtInt
+        call    printf
+        add     esp, 8
+        ret
+
+;------------------------------------------
+; char
+
+mostrarCaracter:                
+        push    dword [caracter]
+        push    fmtChar
+        call    printf
+        add     esp, 8
+        ret
+
+mostrarAuxiliarPrueba:
+        push    dword [auxiliarPrueba]
+        push    fmtChar
+        call    printf
+        add     esp, 8
+        ret
+
 ;------------------------------------------
 ; espacio
+
+mostrarEspacio:
+        push    espacio
+        call    printf
+        add     esp, 4
+        ret
 
 mostrarTabulado:
         push    tabulado
