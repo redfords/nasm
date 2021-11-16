af5fc7d487c7ab5d6734451644dfd3a711ac4318
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-11-16 15:11:10 -0300

    Add sequence

--- ejercicio6.asm
+++ ejercicio6.asm
@@ -2,10 +2,23 @@
 ; Se ingresa N. La computadora muestra los primeros N términos de la Secuencia de
 ; Connell.
 ;
+; 1
+; 2 4
+; 5 7 9
+; 10 12 14 16
+; 17 19 21 23 25
+; 26 28 30 32 34 36
+; 37 39 41 43 45 47 49
+; 50 52 54 56 58 60 62 64
+; 65 67 69 71 73 75 77 79 81
+; 82 84 86 88 90 92 94 96 98 100
+; 101 103 105 107 109 111 113 115 117 119 121
+; 122
+; 
 ; 		nasm -f elf ejercicio6.asm
 ;		ld -m elf_i386 -s -o ejercicio6 ejercicio6.o -lc -I /lib/ld-linux.so.2
 ;		./ejercicio6
-; ----------------------------------------------------------------------------------------
+;----------------------------------------------------------------------------------------
 
 		global	main
 		global	_start
@@ -78,10 +91,6 @@ mostrarSaltoDeLinea:
 		add 	esp, 4
 		ret
 
-salirDelPrograma:
-		push 	0
-		call 	exit
-
 _start:
 main:
 		mov 	edi, 0
@@ -153,4 +162,5 @@ esMayor:
 
 salir:
 		call 	mostrarSaltoDeLinea
-		call 	salirDelPrograma
+		push 	0
+		call 	exit
